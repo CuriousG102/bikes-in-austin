0a0210788dda357e3d2c8648c1dcb3da91940efd
Miles <mileshutson@utexas.edu>
2015-06-06 14:47:19 -0500

    Base work for a slider

--- js/slider.js
+++ js/slider.js
@@ -16,6 +16,56 @@ var InteractiveController = {
 
         console.log('bla');
         $('#slider span').html(moment().subtract(this.DAYS_FROM_PRESENT + this.DAYS_IN_PAST_FOR_DEFAULT, 'days').format('MMMM D, YYYY') + ' - ' + moment().subtract(this.DAYS_FROM_PRESENT, 'days').format('MMMM D, YYYY'));
+
+        this.slider = $("#slider").daterangepicker({
+            minDate: moment().subtract(this.DAYS_IN_THE_PAST, 'days'),
+            maxDate: moment().subtract(this.DAYS_FROM_PRESENT, 'days'),
+            startDate: moment().subtract(this.DAYS_FROM_PRESENT + this.DAYS_IN_PAST_FOR_DEFAULT, 'days'),
+            endDate: moment().subtract(this.DAYS_FROM_PRESENT, 'days'),
+            timePicker: false,
+            dateLimit: {days: this.MAX_RANGE},
+            showDropdowns: true,
+            opens: 'right',
+            ranges: {
+               'Last Week': [moment().subtract('days', 13), moment().subtract('days', 7)],
+               'Last 30 Days': [moment().subtract('days', 36), moment().subtract('days', 7)],
+               'Previous Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+            },
+        });
+
+        // daterangepicker has a bad bug that only sometimes presents and that cuases it to lose its date setting
+        // this should be an ugly but functional workaround. 
+        this.startDate = moment().subtract(this.DAYS_FROM_PRESENT + this.DAYS_IN_PAST_FOR_DEFAULT, 'days');
+        this.endDate = moment().subtract(this.DAYS_FROM_PRESENT, 'days');
+        this.slider
+            .on('apply.daterangepicker', function() {
+                this.startDate = this.slider.data('daterangepicker').startDate;
+                this.endDate = this.slider.data('daterangepicker').endDate;
+                $('#slider span').html(this.startDate.format('MMMM D, YYYY') + ' - ' + this.endDate.format('MMMM D, YYYY'))
+                this.update();
+            }.bind(this));
+    },
+
+    addClient: function(clientFunction) {
+        this.clients.push(clientFunction);
+        // daterangepicker has a bad bug that only sometimes presents and that cuases it to lose its date setting
+        // this should be an ugly but functional workaround.
+        this.slider.data('daterangepicker').setStartDate(this.startDate)
+        this.slider.data('daterangepicker').setEndDate(this.endDate)
+
+        clientFunction(this.slider.data('daterangepicker').startDate.toDate(), 
+                       this.slider.data('daterangepicker').endDate.toDate());
+    },
+
+    update: function() {
+        // daterangepicker has a bad bug that only sometimes presents and that cuases it to lose its date setting
+        // this should be an ugly but functional workaround.
+        this.slider.data('daterangepicker').setStartDate(this.startDate);
+        this.slider.data('daterangepicker').setEndDate(this.endDate);
+
+        for (var i = 0; i < this.clients.length; i++)
+            this.clients[i](this.slider.data('daterangepicker').startDate.toDate(), 
+                            this.slider.data('daterangepicker').endDate.toDate());
     }
 
 }
